747bb6610334b8271ce77ed2915b8db9abfba96d
Kushagra Singh Bisen <k.bisen@yahoo.com>
2021-09-15 11:59:24 +0200

    work done till 15th september 2021

---
+++ Spring/test-spring/src/test/java/com/argahsuknesib/spring/testspring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argahsuknesib.spring.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
